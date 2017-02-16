2d172d745551c9909c6b847cbdf2e004e4419d2b
dcuffari <davidcuffari@yahoo.com>
2017-02-16 17:34:08 -0500

    sim code

---
+++ animate.py
@@ -0,0 +1,40 @@
+
+
+#import matplotlib		#These two lines are only needed
+#matplotlib.use('Agg')		#if you are running this via ssh
+import sys, getopt
+import numpy as np
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as p3
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
+x, y, z = np.loadtxt("new1.dat", delimiter='  ', unpack=True)
+
+line, = ax.plot(x, y, 'yo')
+ax.set_xlim(-2000,2000)
+ax.set_ylim(-2000,2000)
+#ax.set_facecolor('black')
+
+
+def animate(i):
+    x, y, z = np.loadtxt("new" + str(i+1) + ".dat", delimiter='  ', unpack=True)
+    line.set_label("test" + str(i) )
+    plt.title("Time " + str(i))
+    line.set_data(x, y)
+    return line, 
+
+
+
+# Init only required for blitting to give a clean slate.
+def init():
+    line.set_ydata(np.ma.array(x, mask=True))
+    return line,
+
+ani = animation.FuncAnimation(fig, animate, np.arange(1, 3000), init_func=init,
+                              interval=25, blit=True)
+
+
+
+#ani.save('gravAni2.mp4')
+plt.show()
