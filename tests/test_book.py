dc6c7e912af2c32e42fae9eb03d38897cc029e08
dcuffari <davidcuffari@yahoo.com>
2017-03-29 13:35:17 -0400

    I've added a test for the remove_i function.

--- tests/test_book.py
+++ tests/test_book.py
@@ -1,5 +1,9 @@
-from book.book import *
 import pytest
 
 
-def test_book()
+def test_book():
+    from book.book import remove_i
+    import numpy as np
+    x = np.zeros((5, 3), dtype=float)
+    y = remove_i(x, 2)
+    assert np.size(x) == np.size(y) + 3
