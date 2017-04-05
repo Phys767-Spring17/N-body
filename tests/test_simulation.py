6a12a2b40d403bcdfcf5d151bbd4fb805b35a181
dcuffari <davidcuffari@yahoo.com>
2017-04-05 13:42:05 -0400

    more cleaning

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -10,9 +10,17 @@ import pytest
 #    from simulation.gravsim import simulate as sim
 #    assert sim(test, 1, 1, 1, 1, 1) == '\nSimulation complete. Your data has been saved as ' + sys.argv[1] + '*.dat\n'
 
-def test_ic_7():
-    from simulation.gravsim import input_check as ic
-    with self.assertRaises(SystemExit) as cm:
-        ic(5)
+#def test_ic_7(self):
+#    from simulation.gravsim import input_check as ic
+#    with self.assertRaises(SystemExit) as cm:
+#        self.ic(5)
+#    self.assertEqual(cm.exception.code, 2)
 
-    self.assertEqual(cm.exception.code, 2)
+class test(object):
+    def test_ic_7(self):
+        from simulation.gravsim import input_check as ic
+        with self.assertRaises(SystemExit) as cm:
+            self.ic(5)
+#       assert cm.exception.code == "SystemExit"
+        self.assertEqual(cm.exception.error_code, 2)
+
