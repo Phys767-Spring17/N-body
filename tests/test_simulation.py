370f5f662f59f53f5a5653de2886432ec78cd308
dcuffari <davidcuffari@yahoo.com>
2017-04-05 11:49:51 -0400

    Attempting to remove files that aren't needed.

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -6,7 +6,13 @@ import pytest
 #    assert ic(7) == 'test', 1, 1, 1, 1, 1
 
 
-def test_sim():
-    from simulation.gravsim import simulate as sim
-    assert sim(test, 1, 1, 1, 1, 1) == '\nSimulation complete. Your data has been saved as ' + sys.argv[1] + '*.dat\n'
+#def test_sim():
+#    from simulation.gravsim import simulate as sim
+#    assert sim(test, 1, 1, 1, 1, 1) == '\nSimulation complete. Your data has been saved as ' + sys.argv[1] + '*.dat\n'
 
+def test_ic_7():
+    from simulation.gravsim import input_check as ic
+    with self.assertRaises(SystemExit) as cm:
+        ic(5)
+
+    self.assertEqual(cm.exception.code, 2)
