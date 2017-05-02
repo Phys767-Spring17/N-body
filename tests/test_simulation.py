cab4a093be8fa656ae95e38a24ecab10ac1ff304
dcuffari <davidcuffari@yahoo.com>
2017-05-02 15:50:24 -0400

    updated test_simulation

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -2,13 +2,16 @@ import pytest
 
 
 #def test_ic_7():
+#    from simulation.gravsim import input_check as ic
+#    sys.argv = ["gravsim.py", "test", 1, 1, 1, 1, 1]
 #    filename, N, D, S, G, dt = ic(7)
 #    assert ic(7) == 'test', 1, 1, 1, 1, 1
-
+#    assert filename == "test"
 
 #def test_sim():
 #    from simulation.gravsim import simulate as sim
-#    assert sim(test, 1, 1, 1, 1, 1) == '\nSimulation complete. Your data has been saved as ' + sys.argv[1] + '*.dat\n'
+#    test = sim(test, 1, 1, 1, 1, 1)
+#    assert test = '\nSimulation complete. Your data has been saved as ' + sys.argv[1] + '*.dat\n'
 
 #def test_ic_7(self):
 #    from simulation.gravsim import input_check as ic
@@ -16,11 +19,11 @@ import pytest
 #        self.ic(5)
 #    self.assertEqual(cm.exception.code, 2)
 
-class test(object):
-    def test_ic_7(self):
-        from simulation.gravsim import input_check as ic
-        with self.assertRaises(SystemExit) as cm:
-            self.ic(5)
+#class test(object):
+#    def test_ic_7(self):
+#        from simulation.gravsim import input_check as ic
+#        with self.assertRaises(SystemExit) as cm:
+#            self.ic(5)
 #       assert cm.exception.code == "SystemExit"
-        self.assertEqual(cm.exception.error_code, 2)
+#        self.assertEqual(cm.exception.error_code, 2)
 
