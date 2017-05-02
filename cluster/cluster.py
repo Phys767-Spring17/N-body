4cab36a0f39830720ef80ade79e56260f6d0122a
dcuffari <davidcuffari@yahoo.com>
2017-05-02 16:53:33 -0400

    I'm slowling adding the new cluster features to the simulation.

--- cluster/cluster.py
+++ cluster/cluster.py
@@ -10,14 +10,23 @@ class cluster(object):
     
     roar = "I am a cluster!\n"
     def __init__(self, partnum, clusnum, D, M):
-        """
+        """initilizes the object with the follow:
+        
+        param particle_cout: the number of particles in the simulation.
+        param particle_dimensions: the number of dimensions.
+        param cluster_count: the number of particle clusters.
+        param cluster_dimensions: the number of dimensions
+        param particle_positions: the x,y,z position of the particles
+        param particle_velocities:
+        param particle_masses:
+        param cluster_position:
         """
         self.particle_count = partnum
         self.particle_dimensions = D
         self.cluster_count = clusnum
         self.cluster_dimensions = D
         self.particle_positions, self.particle_velocities, self.particle_masses = book.initial_cond(partnum, D)
-        self.cluster_position = np.random.rand(clusnum, D)*M
+        self.cluster_position = np.random.rand(1, D)*M
         
         
     def hear_me(self):
@@ -28,6 +37,13 @@ class cluster(object):
             "  My cluster count is:           " + str(self.cluster_count) + "\n" +
             "  My first particle position is: x: " + str(self.particle_positions[0,0]) + "\n" +
             "                                 y: " + str(self.particle_positions[0,1]) + "\n" +
-            "                                 z: " + str(self.particle_positions[0,2]))
- 
+            "                                 z: " + str(self.particle_positions[0,2]) + "\n" +
+            "  My cluster count is:              " + str(self.cluster_count) + "\n" +
+            "  My cluster position is:           " + str(self.cluster_position) + "\n")
         print(myroar)
+
+    def cluster(self):
+        x0 = self.particle_positions + self.cluster_position
+        v0 = self.particle_velocities
+        m = self.particle_masses
+        return x0, v0, m
