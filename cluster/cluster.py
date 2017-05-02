bb8fc1d8083a1eccbecfc70af7a3b61393a8ea5e
dcuffari <davidcuffari@yahoo.com>
2017-05-02 10:38:28 -0400

    I've created a cluster class based on the book example.

---
+++ cluster/cluster.py
@@ -0,0 +1,48 @@
+import numpy as np
+import book as book
+
+
+class cluster(object):
+    """A cluster is comprised of N unity masses
+       starting at a random position in D-dimensional spce.
+       
+    """
+    
+    roar = "I am a cluster!\n"
+    
+    def __init__(self, partnum, clusnum, D, M):
+        """
+        """
+        self.particle_count = partnum
+        self.particle_dimensions = D
+        self.cluster_count = clusnum
+        self.cluster_dimensions = D
+        self.particle_positions = book.initial_cond(partnum, D)
+        self.cluster_position = np.random.rand(clusnum, D)*M
+        
+        
+    def hear_me(self):
+        myroar = self.roar + (
+            " My particle count is:          " + str(self.particle_count) + "\n" +
+            " My particle dimensions are:    " + str(self.particle_dimensions) + "\n" +
+            " My cluster count is:           " + str(self.cluster_count) + "\n" +
+            " My first particle position is: " + str(self.particle_positions[0]) + "\n" +
+            "                                " + str(self.particle_positions[1]) + "\n" +
+            "                                " + str(self.particle_positions[2]))
+        print(myroar)
+
+        
+#    def cluster(self, N, D):
+#        """
+#        """
+#        self.particle_count = N
+#        self.particle_dimensions = D
+#        self.particle_positions = book.initial_cond(N, D)
+#     
+            
+#    def cluster_position(self, N, D, M):
+#        """
+#        """
+#        self.cluster_count = N
+#        self.cluster_dimensions = D
+#        self.cluster_position = np.random.rand(N, D)*M
