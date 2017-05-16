dc49f29a130e8269373cb8b919564d25f80b07fc
dcuffari <davidcuffari@yahoo.com>
2017-05-16 10:27:10 -0400

    finished and cleaned paraBook.py

--- parallel/threads/paraBook.py
+++ parallel/threads/paraBook.py
@@ -1,10 +1,8 @@
-#!/usr/bin/python   ##Shabang!
-
 from threading import Thread
 import numpy as np
 
 def remove_i(x, i):
-    """
+    """Drops the ith element of an array.
     
     """
     shape = (x.shape[0]-1,) + x.shape[1:]
@@ -13,32 +11,18 @@ def remove_i(x, i):
     y[i:] = x[i+1:]
     return y
 
+
 def a(i, x, G, m):
-    """
+    """The acceleration of the ith mass.
     
     """
     x_i = x[i]
     x_j = remove_i(x, i)
     m_j = remove_i(m, i)
     diff = x_j - x_i
-    if np.any(diff) < 1:
-        diff = 1
-    else:
-        mag3 = np.sum(diff**2, axis=1)**1.5
+    mag3 = np.sum(diff**2, axis=1)**1.5
     result = G * np.sum(diff * (m_j / mag3)[:,np.newaxis], axis=0)
     return result
-
-def initial_cond(N, D):
-    """
-    
-    """
-    x0 = np.random.rand(N, D)-250
-    for i in range(N/2):
-        for j in range(D):
-            x0[i,j] = x0[i,j]+500
-    v0 = np.zeros((N, D), dtype=float)
-    m = np.ones(N, dtype=float)
-    return x0, v0, m
 
 
 def timestep(x0, v0, G, m, dt, pool):
@@ -55,6 +39,19 @@ def timestep(x0, v0, G, m, dt, pool):
         x1[i] = x_i1
         v1[i] = v_i1
     return x1, v1
+
+
+def initial_cond(N, D):
+    """Generates initial conditions for N unity masses at rest
+       starting at random positions in D-dimensional space.
+    """
+    x0 = np.random.rand(N, D)-250
+    for i in range(N/2):
+        for j in range(D):
+            x0[i,j] = x0[i,j]+500
+    v0 = np.zeros((N, D), dtype=float)
+    m = np.ones(N, dtype=float)
+    return x0, v0, m
 
 
 class Worker(Thread):
