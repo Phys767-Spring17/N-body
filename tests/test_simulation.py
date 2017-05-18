81d9633c929da700ffbe9a15a29e51896d28810f
dcuffari <davidcuffari@yahoo.com>
2017-05-18 17:17:19 -0400

    I created new tests for gravsim.py

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -1,29 +1,16 @@
 import pytest
 
 
-#def test_ic_7():
-#    from simulation.gravsim import input_check as ic
-#    sys.argv = ["gravsim.py", "test", 1, 1, 1, 1, 1]
-#    filename, N, D, S, G, dt = ic(7)
-#    assert ic(7) == 'test', 1, 1, 1, 1, 1
-#    assert filename == "test"
+def test_ic_7():
+    from simulation.gravsim import input_check as ic
+    import sys
+    sys.argv = ["gravsim.py", "test", 1, 3, 1, 1, 1]
+    filename, N, D, S, G, dt = ic(7)
+    assert filename == "test"
 
-#def test_sim():
-#    from simulation.gravsim import simulate as sim
-#    test = sim(test, 1, 1, 1, 1, 1)
-#    assert test = '\nSimulation complete. Your data has been saved as ' + sys.argv[1] + '*.dat\n'
-
-#def test_ic_7(self):
-#    from simulation.gravsim import input_check as ic
-#    with self.assertRaises(SystemExit) as cm:
-#        self.ic(5)
-#    self.assertEqual(cm.exception.code, 2)
-
-#class test(object):
-#    def test_ic_7(self):
-#        from simulation.gravsim import input_check as ic
-#        with self.assertRaises(SystemExit) as cm:
-#            self.ic(5)
-#       assert cm.exception.code == "SystemExit"
-#        self.assertEqual(cm.exception.error_code, 2)
-
+def test_sim():
+    from simulation.gravsim import input_check as ic
+    import sys
+    sys.argv = ["gravsim.py", "test", 1, 3, 1, 1, 1]
+    filename, N, D, S, G, dt = ic(7)
+    assert N == 1, D == 3
