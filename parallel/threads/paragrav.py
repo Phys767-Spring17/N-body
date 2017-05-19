1f9f6d6107117339814ea89fc1af298956f88d0c
dcuffari <davidcuffari@yahoo.com>
2017-05-18 21:17:38 -0400

    I updated paragrav.py to include try except raise style user error handling.

--- parallel/threads/paragrav.py
+++ parallel/threads/paragrav.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python   ##Shabang!
+#!/usr/bin/python  ##Shabang!
 
-# import matplotlib		# These two lines are only needed
-# matplotlib.use('Agg')		# if you are running this via ssh
+# import matplotlib  # These two lines are only needed
+# matplotlib.use('Agg')  # if you are running this via ssh
 
 import paraBook as book  # functions taken from book packaged as a module.
 import sys  # modules used to fancify user interface.
-import os  # modules used to fancify user interface.
 
 
 def input_check(n):
@@ -17,45 +16,97 @@ def input_check(n):
     :return: params for simulations.
     """
     if len(sys.argv) == n:
-        filename = sys.argv[1]
-        P = sys.argv[2]
-        N = sys.argv[2]
-        D = sys.argv[3]
-        S = sys.argv[4]
-        G = sys.argv[5]
-        dt = sys.argv[6]
-        return filename, N, D, S, G, dt
-    else:
-        print('\n    The proper use of gravsim.py is as follows.')
-        print('\n        python gravsim.py outputfile N D S G dt\n')
-        print('    Simulate function from Computation book modified to take in\n'
-               "    user specified variables N, D, S, G, dt and output a user specified\n"
-                "    file name.\n"
+     filename = sys.argv[1]
+     P = sys.argv[2]
+     N = sys.argv[3]
+     D = sys.argv[4]
+     S = sys.argv[5]
+     G = sys.argv[6]
+     dt = sys.argv[7]
+     return filename, P, N, D, S, G, dt
 
-                "    :param N: The number of particles.\n"
-                "    :param D: The number of dimensions.\n"
-                "    :param S: The number of time steps.\n"
-                "    :param G: Gravitational constant.\n"
-                "    :param dt: The time step.\n")
-        os._exit(1)
-    
 
-def input_int_check(N, D, S):
+def input_int_check(P, N, D, S):
     """conditional check for integer values of N, D, and S.
-    
+
+    :param P: The number of threads.
     :param N: The number of particles in the simulation.
     :param D: The number of dimensions.
     :param S: The number of time steps to be used for the simulation.
     :return: N/A
     """
-    for i in range(6):
-        i = 2
-        test = sys.argv[i]
-        if test.isdigit():
-            continue
-        else:
-            print('\nN, D, and S must be integers\n')
-            os._exit(1)
+    P.isdigit()
+    N.isdigit()
+    D.isdigit()
+    S.isdigit()
+
+
+def usage_error():
+    """Prints to screen how to use the program when the user raises an error.
+    
+    """
+    print('\n    The proper use of gravsim.py is as follows.')
+    print('\n        python gravsim.py outputfile N D S G dt\n')
+    print('    Simulate function from Computation book modified to take in\n'
+           "    user specified variables P, N, D, S, G, dt and output a user specified\n"
+           "    file name.\n"
+
+           "    :param P: The number of threads.\n"
+           "    :param N: The number of particles.\n"
+           "    :param D: The number of dimensions.\n"
+           "    :param S: The number of time steps.\n"
+           "    :param G: Gravitational constant.\n"
+           "    :param dt: The time step.\n")
+
+
+#def input_check(n):
+#    """run time argument handling of user defined values.
+#    A total of 7 arguments must be included at run time
+#    as defined by the error message listed below.
+
+#    :param n: number of command line arguments
+#    :return: params for simulations.
+#    """
+#    if len(sys.argv) == n:
+#        filename = sys.argv[1]
+#        P = sys.argv[2]
+#        N = sys.argv[2]
+#        D = sys.argv[3]
+#        S = sys.argv[4]
+#        G = sys.argv[5]
+#        dt = sys.argv[6]
+#        return filename, N, D, S, G, dt
+#    else:
+#        print('\n    The proper use of gravsim.py is as follows.')
+#        print('\n        python gravsim.py outputfile N D S G dt\n')
+#        print('    Simulate function from Computation book modified to take in\n'
+#               "    user specified variables N, D, S, G, dt and output a user specified\n"
+#                "    file name.\n"
+
+#                "    :param N: The number of particles.\n"
+#                "    :param D: The number of dimensions.\n"
+#                "    :param S: The number of time steps.\n"
+#                "    :param G: Gravitational constant.\n"
+#                "    :param dt: The time step.\n")
+#        os._exit(1)
+    
+
+#def input_int_check(N, D, S):
+#    """conditional check for integer values of N, D, and S.
+    
+#    :param N: The number of particles in the simulation.
+#    :param D: The number of dimensions.
+#    :param S: The number of time steps to be used for the simulation.
+#    :return: N/A
+#    """
+#    for i in range(6):
+#        i = 2
+#        test = sys.argv[i]
+#        if test.isdigit():
+#            continue
+#        else:
+#            print('\nN, D, and S must be integers\n')
+#            os._exit(1)
           
 
 def simulate(P, N, D, S, G, dt):
@@ -83,7 +134,24 @@ def simulate(P, N, D, S, G, dt):
     return '\nSimulation complete. Your data has been saved as ' + sys.argv[1] + '*.dat\n'
 
 
-filename, P, N, D, S, G, dt = input_check(8)
-input_int_check(N, D, S)
 
-print(simulate(int(P) ,int(N), int(D), int(S), float(G), float(dt)))  # get the party started!
+#filename, P, N, D, S, G, dt = input_check(8)
+#input_int_check(N, D, S)
+
+#print(simulate(int(P) ,int(N), int(D), int(S), float(G), float(dt)))  # get the party started!
+
+
+while True:
+    try:
+        filename, P, N, D, S, G, dt = input_check(8)
+        input_int_check(P, N, D, S)
+        print(simulate(int(P), int(N), int(D), int(S), float(G), float(dt)))
+        break
+    except(TypeError, NameError):
+        print('\n    TypeError:  You did not provide enough input values.\n')
+        usage_error()
+        break
+    except ValueError:
+        print('\n    ValueError: P, N, D, and S must be integers\n')
+        usage_error()
+        break
