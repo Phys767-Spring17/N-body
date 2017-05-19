22c604ee1e521047b8ab9c07f4204e24e4f4aed3
dcuffari <davidcuffari@yahoo.com>
2017-05-19 10:38:34 -0400

    changed the way data is read into removelarge.py

--- visualization/density_plots/removelarge.py
+++ visualization/density_plots/removelarge.py
@@ -1,8 +1,15 @@
 import numpy as np
+import sys
 
-x, y, z = np.loadtxt("temp1000.data", delimiter=' ', unpack=True)
+filename = sys.argv[1]
 
-with open("proctemp1000.data", "w") as myfile:
+data = np.loadtxt(filename)
+
+x = data[:,0]
+y = data[:,1]
+z = data[:,2]
+
+with open("proc_" + filename, "w") as myfile:
     for i in range(len(x)):
         if x[i] < 600 and x[i] > -100 and y[i] < 600 and y[i] > -100 and z[i] < 600 and z[i] > -100:
             myfile.write(str(x[i]) + " " + str(y[i]) + " "  + str(z[i]) + "\n")
